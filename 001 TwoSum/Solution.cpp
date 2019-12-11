9cabaa582fbffb8b0f32f3cc81c4334dae015782
graveszhang <graves_zhang@sjtu.edu.cn>
2019-12-11 15:00:15 +0800

    solution added

---
+++ 001 TwoSum/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i=0;i<nums.size();++i){
+            for (int j=i+1;j<nums.size();++j){
+                if (nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+    return result;
+    }
+};
+
+//执行用时 : 452 ms, 在所有 cpp 提交中击败了10.08% 的用户
+//内存消耗 : 9 MB, 在所有 cpp 提交中击败了98.74%的用户
