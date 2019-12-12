a5a304f008b4e8cb04e9266e0a8dd8ae329983cf
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2019-12-12 18:14:48 +0800

    Update Solution.cpp

--- 001 TwoSum/Solution.cpp
+++ 001 TwoSum/Solution.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
@@ -14,5 +14,27 @@ public:
     }
 };
 
-//执行用时 : 452 ms, 在所有 cpp 提交中击败了 10.08% 的用户
-//内存消耗 : 9 MB, 在所有 cpp 提交中击败了 98.74% 的用户
+// 执行用时 : 452 ms, 在所有 cpp 提交中击败了 10.08% 的用户
+// 内存消耗 : 9 MB, 在所有 cpp 提交中击败了 98.74% 的用户
+
+class Solution2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> a; // hash
+        vector<int> b(2,-1); // 初始化一个大小为2，值为-1的容器b
+        for(int i=0;i<nums.size();i++)
+        {
+            if(a.count(target-nums[i])>0){ // 判断需要的值是否在数组中出现超过0次
+                b[0]=a[target-nums[i]];
+                b[1]=i;
+                break;
+            }
+            a.insert({nums[i],i});
+            // a[nums[i]]=i
+            // 这个map的key是数组的值，value是对应的index
+        }
+        return b;
+    };
+};
+// 执行用时 : 12 ms, 在所有 cpp 提交中击败了 87.61% 的用户
+// 内存消耗 : 10 MB, 在所有 cpp 提交中击败了 42.82% 的用户
