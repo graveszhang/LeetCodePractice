f157aa4ccdd2408e44d17a0f6553c8507ec1be01
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2019-12-15 02:03:07 +0800

    Update Solution.cpp

--- 003 Longest Substring Without Repeating Character/Solution.cpp
+++ 003 Longest Substring Without Repeating Character/Solution.cpp
@@ -1,42 +1,26 @@
-class Solution1 {
+class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for (int i=0;i<nums.size();++i){
-            for (int j=i+1;j<nums.size();++j){
-                if (nums[i]+nums[j]==target){
-                    result.push_back(i);
-                    result.push_back(j);
-                    break;
+    int lengthOfLongestSubstring(string s) {
+        int left=0;
+        int right=1;
+        int length=0;
+        while (right<s.length()+1){
+            for (int i=left; i<right; i++){
+                if (s[i]==s[right]){
+                    if (right-left>length){
+                        length=right-left;
+                    }
+                    left=i+1;
                 }
             }
-        }
-    return result;
-    }
-};
-
-// 执行用时 : 452 ms, 在所有 cpp 提交中击败了 10.08% 的用户
-// 内存消耗 : 9 MB, 在所有 cpp 提交中击败了 98.74% 的用户
-
-class Solution2 {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> a; // hash
-        vector<int> b(2,-1); // 初始化一个大小为2，值为-1的容器b
-        for(int i=0;i<nums.size();i++)
-        {
-            if(a.count(target-nums[i])>0){ // 判断需要的值是否在数组中出现超过0次
-                b[0]=a[target-nums[i]];
-                b[1]=i;
-                break;
+            if (right-left>length){
+                length=right-left;
             }
-            //a.insert({nums[i],i});
-            //insert函数插入相同的key不会覆盖value，但下面这句可以
-            a[nums[i]]=i;
-            // 这个map的key是数组的值，value是对应的index
+            right++;                     
         }
-        return b;
-    };
+        return length;                         
+    }                           
 };
-// 执行用时 : 12 ms, 在所有 cpp 提交中击败了 87.61% 的用户
-// 内存消耗 : 10 MB, 在所有 cpp 提交中击败了 42.82% 的用户
+
+// 执行用时 : 24 ms, 在所有 cpp 提交中击败了 55.02% 的用户
+// 内存消耗 : 9.1 MB, 在所有 cpp 提交中击败了 90.39% 的用户
