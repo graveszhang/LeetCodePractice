d6071d0b0993eeb82c5f3b166196af6ebc545e1d
graveszhang <graves_zhang@sjtu.edu.cn>
2020-01-18 15:35:52 +0800

    solution 1

--- 008 String to integer(atoi)/Solution.py
+++ 008 String to integer(atoi)/Solution.py
@@ -1,12 +1,30 @@
-str = "-123"
-## above line only for testing
-
-def myAtoi(self, str: str) -> int:
-    print(str)
-    print(str[0])
-    return 0
+def myAtoi(str: str) -> int:
+    flag = False
+    result = []
+    for c in str:
+        if (c == ' '):
+            continue
+        elif (c == '-'):
+            flag = True
+        elif (c.isdigit()):
+            result.append(int(c))
+        else:
+            if(result==[]): return 0
+            else: continue
+    
+    times=1
+    intResult = 0
+    for i in reversed(result):
+        intResult += i*times
+        times*=10
+    
+    if flag: intResult = -1 * intResult
+    
+    if (intResult <= -2**31): return -2**31
+    elif (intResult >= 2**32-1): return 2**32-1
+    else: return intResult
 
 if (__name__ == "__main__"):
-    result = myAtoi(str=str)
-    print(result)
+    str="-123 dsa"
+    print(myAtoi(str))
 
