44059ce095b87859cab6581f17ffb8931431408e
graveszhang <graves_zhang@sjtu.edu.cn>
2020-01-18 14:51:16 +0800

    add 008

---
+++ 008 String to integer(atoi)/Solution.py
@@ -0,0 +1,12 @@
+str = "-123"
+## above line only for testing
+
+def myAtoi(self, str: str) -> int:
+    print(str)
+    print(str[0])
+    return 0
+
+if (__name__ == "__main__"):
+    result = myAtoi(str=str)
+    print(result)
+
