70e6fe55838093a6b6edd2a7612beaefcc46fc5b
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-01-18 16:36:06 +0800

    Create Solution2.py

---
+++ 008 String to integer(atoi)/Solution2.py
@@ -0,0 +1,9 @@
+import re
+class Solution:
+    def myAtoi(self, str: str) -> int:
+		if not re.findall('^\s*([-+]?[0-9]+)',str):
+            return 0
+        else: intResult = int(re.findall('^\s*([-+]?[0-9]+)',str)[0])
+		if (intResult <= -2**31): return -2**31
+        elif (intResult >= 2**31-1): return 2**31-1
+        else: return intResult
