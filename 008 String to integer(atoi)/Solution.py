103121fbb9f0898cf82f6bb754107b468d1bd78d
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-01-18 15:56:52 +0800

    Update Solution.py

--- 008 String to integer(atoi)/Solution.py
+++ 008 String to integer(atoi)/Solution.py
@@ -1,28 +1,34 @@
-def myAtoi(str: str) -> int:
-    flag = False
-    result = []
-    for c in str:
-        if (c == ' '):
-            continue
-        elif (c == '-'):
-            flag = True
-        elif (c.isdigit()):
-            result.append(int(c))
-        else:
-            if(result==[]): return 0
-            else: continue
-    
-    times=1
-    intResult = 0
-    for i in reversed(result):
-        intResult += i*times
-        times*=10
-    
-    if flag: intResult = -1 * intResult
-    
-    if (intResult <= -2**31): return -2**31
-    elif (intResult >= 2**32-1): return 2**32-1
-    else: return intResult
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        flag = False
+        signFlag = 0
+        result = []
+        for c in str:
+            if (c == ' '): 
+                if (result == [] and signFlag==0): continue
+                else: break
+            elif (c == '+' and result == []):
+                flag = False
+                signFlag += 1
+            elif (c == '-' and result == []):
+                flag = True
+                signFlag += 1
+            elif (c.isdigit()):
+                result.append(int(c))
+            else: break
+        if (signFlag > 1): return 0
+        
+        times=1
+        intResult = 0
+        for i in reversed(result):
+            intResult += i*times
+            times*=10
+
+        if flag: intResult = -1 * intResult
+
+        if (intResult <= -2**31): return -2**31
+        elif (intResult >= 2**31-1): return 2**31-1
+        else: return intResult
 
 if (__name__ == "__main__"):
     str="-123 dsa"
