e305b535c2278d007baf249404fc5bb68048e0bc
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-01-31 21:04:22 +0800

    Create Solution.py

---
+++ 014 Longest Common Prefix/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: return ""
+        if len(strs)==1: return strs[0]
+        minlen = 1000
+        for i in strs:
+            minlen = min(len(i),minlen) 
+            if not i: 
+                return "" 
+        common = ""
+        pt = 0
+        while (pt<minlen):
+            common += strs[0][pt]
+            for i in strs:
+                if (common == i[:pt+1:]):
+                    continue
+                else:
+                    return common[:pt:]
+            pt += 1
+        return common
