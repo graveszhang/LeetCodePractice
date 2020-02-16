46e8c93964e13d49bd59b8d2b4ef0f768bccba9d
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-02-16 21:02:49 +0800

    Update Solution.py

--- 021 Merge Two Sorted List/Solution.py
+++ 021 Merge Two Sorted List/Solution.py
@@ -17,4 +17,17 @@ class Solution:
             l1 = l1.next
         return head
 
+class Solution:
+    ### Recursion
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
