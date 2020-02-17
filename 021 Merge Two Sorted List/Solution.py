2b2d1f3943aa8c495531926a669351df59ba829b
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-02-17 23:40:47 +0800

    Update Solution.py

--- 021 Merge Two Sorted List/Solution.py
+++ 021 Merge Two Sorted List/Solution.py
@@ -4,19 +4,36 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
-    #### Not Passed --> Time Limited Err
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if l2 == None: return l1
-        head = l2
+        if l1 == None: return l2
+        head=ListNode(0);
+        if (l1.val < l2.val): 
+            head.next = l1
+            l1 = l2
+            l2 = head.next
+        else: head.next = l2
+        prev = head.next
         while l1 != None:
-            while l2.next.val <= l1.val:
-                l2 = l2.next
-            l1.next = l2.next
-            l2.next = l1 
-            l1 = l1.next
-        return head
-
+            flag = False
+            while l2.val <= l1.val:
+                if l2.next == None:
+                    l2.next = l1
+                    flag = True
+                    break
+                else:
+                    prev = l2
+                    l2 = l2.next
+            if flag: break
+            tmp = l1.next
+            l1.next = l2
+            prev.next = l1 
+            prev = prev.next
+            l1 = tmp 
+        return head.next
+    
 class Solution:
     ### Recursion
     def mergeTwoLists(self, l1, l2):
