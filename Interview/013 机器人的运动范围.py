517dcda45c4511b7b8d81af27bdd02bc41d1c455
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-04-08 16:13:40 +0800

    Update 013 机器人的运动范围.py

--- Interview/013 机器人的运动范围.py
+++ Interview/013 机器人的运动范围.py
@@ -30,3 +30,20 @@ class Solution:
                 for nx, ny in [(x + 1, y), (x, y + 1)]:
                     q.put((nx, ny))
         return len(s)
+
+    def digitsum(n):
+    ans = 0
+    while n:
+        ans += n % 10
+        n //= 10
+    return ans
+
+## dp
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        vis = set([(0, 0)])
+        for i in range(m):
+            for j in range(n):
+                if ((i - 1, j) in vis or (i, j - 1) in vis) and digitsum(i) + digitsum(j) <= k:
+                    vis.add((i, j))
+        return len(vis)
