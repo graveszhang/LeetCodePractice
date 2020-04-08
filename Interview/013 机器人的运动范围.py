bd8bb42c508725e17792897472373e4b656baed8
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-04-08 16:09:08 +0800

    Update 013 机器人的运动范围.py

--- Interview/013 机器人的运动范围.py
+++ Interview/013 机器人的运动范围.py
@@ -1,3 +1,4 @@
+## dfs
 class Solution:
     def movingCount(self, m: int, n: int, k: int) -> int:
         def dfs(i, j, si, sj):
@@ -7,3 +8,25 @@ class Solution:
 
         visited = set()
         return dfs(0, 0, 0, 0)
+
+## bfs
+def digitsum(n):
+    ans = 0
+    while n:
+        ans += n % 10
+        n //= 10
+    return ans
+
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        from queue import Queue
+        q = Queue()
+        q.put((0, 0))
+        s = set()
+        while not q.empty():
+            x, y = q.get()
+            if (x, y) not in s and 0 <= x < m and 0 <= y < n and digitsum(x) + digitsum(y) <= k:
+                s.add((x, y))
+                for nx, ny in [(x + 1, y), (x, y + 1)]:
+                    q.put((nx, ny))
+        return len(s)
