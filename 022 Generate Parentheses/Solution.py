e600ad5e730d7055fb59d6ec961d775ec408cb10
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-04-09 19:57:43 +0800

    Create Solution.py

---
+++ 022 Generate Parentheses/Solution.py
@@ -0,0 +1,24 @@
+##dfs
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        cur_str = ''
+
+        def dfs(cur_str, left, right):
+            """
+            left: 左括号还可以使用的个数
+            right: 右括号还可以使用的个数
+            """
+            if left == 0 and right == 0:
+                res.append(cur_str)
+                return
+            if right < left:
+                return
+            if left > 0:
+                dfs(cur_str + '(', left - 1, right)
+            if right > 0:
+                dfs(cur_str + ')', left, right - 1)
+
+        dfs(cur_str, n, n)
+        return res
