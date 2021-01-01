e3d98f3bad87a3405e72f58c95fa2de25fea521f
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2021-01-01 15:51:09 +0800

    Update Solution.java

--- 003 Longest Substring Without Repeating Character/Solution.java
+++ 003 Longest Substring Without Repeating Character/Solution.java
@@ -5,6 +5,8 @@ class Solution {
         int len = 0;
         for (int i = 0; i < s.length(); i++){
             if (HashTable.containsKey(s.charAt(i)) && HashTable.get(s.charAt(i)) > head)
+                // 后半条件句用来判断重复字母是否在当前窗口中
+                // case: "tmmzuxt"
                 head = HashTable.get(s.charAt(i));
             else len = Math.max((i - head), len);
             HashTable.put(s.charAt(i),i);
