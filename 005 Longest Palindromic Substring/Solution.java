d980c22df2dd2b2c86493369d792ba0ad94f5eb7
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2021-01-02 23:46:40 +0800

    Update Solution.java

--- 005 Longest Palindromic Substring/Solution.java
+++ 005 Longest Palindromic Substring/Solution.java
@@ -25,3 +25,37 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n < 2) return s;
+        int max = -1;
+        String res = "";
+        for (int i = 0; i < n - 1; i++){
+            String odd = centerSpread(s, i, i);
+            String even = centerSpread(s, i, i + 1);
+            String maxstr = odd.length() > even.length() ? odd : even;
+            if (maxstr.length() > maxLen) {
+                maxLen = maxstr.length();
+                res = maxstr;
+            }
+        }
+        return res;
+    }
+
+    private String centerSpread(String s, int left, int right) {
+        int len = s.length();
+        int i = left;
+        int j = right;
+        while (i >= 0 && j < len) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
+            } else {
+                break;
+            }
+        }
+        return s.substring(i + 1, j);
+    }
+}
