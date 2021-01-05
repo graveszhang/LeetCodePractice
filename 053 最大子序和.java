83f55de77fccba24f8930baeeefb14086658ec28
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2021-01-05 21:18:34 +0800

    Update 053 最大子序和.java

--- 053 最大子序和.java
+++ 053 最大子序和.java
@@ -12,3 +12,14 @@ class Solution {
         return maxx;
     }
 }
+// 由于dp[i]只和dp[i-1]有关 可以用一个变量记录 这样空间复杂度从O(N)降低到了O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
