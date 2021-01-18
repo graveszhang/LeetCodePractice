ff5df260e596b0472ff00a5d8688a27a695354b2
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2021-01-18 22:34:51 +0800

    Create Dp.java

---
+++ Dynamic Programming/121 买卖股票的最佳时机/Dp.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        int[][] dp = new int[2][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] + prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], -prices[i]);
+        }
+        return dp[(len - 1) & 1][0];
+    }
+}
+
+作者：liweiwei1419
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/bao-li-mei-ju-dong-tai-gui-hua-chai-fen-si-xiang-b/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
