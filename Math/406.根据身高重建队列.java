94ff840fe3af2c6a707a220c45991a0518090e6f
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2021-03-08 16:30:50 +0800

    Create 406.根据身高重建队列.java

---
+++ Math/406.根据身高重建队列.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int[] i : people) {
+            list.add(i[1], i);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+}
